95780d7de0a4375c0e4b26660d99e0326f916ac1
vinicius-pc-home <viniciussilvano022@gmail.com>
2023-07-19 11:28:23 -0300

    JPA composicao usando embeddable

---
+++ exercicios-jpa/src/main/java/modelo/consulta/ObterMediaDosFilmes.java
@@ -0,0 +1,19 @@
+package modelo.consulta;
+
+import infra.DAO;
+import modelo.muitospramuitos.Filme;
+
+public class ObterMediaDosFilmes {
+
+	public static void main(String[] args) {
+		
+		DAO<NotaFilme> dao = new DAO<>(NotaFilme.class);
+		NotaFilme nota = dao.consultarUm("obterMediaGeralDosFilmes");
+		
+		System.out.println(nota.getMedia());
+		
+		dao.fechar();
+		
+	}
+	
+}
